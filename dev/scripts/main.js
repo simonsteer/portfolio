f0d2cb48bd25f18dc2d6f14c7119934f4eb643ee
Simon Steer <simonpsteer@gmail.com>
2017-12-09 17:51:02 -0500

    completed most of styling, beginning js work on header transition

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,16 +1,49 @@
-var config = {
-  apiKey: "AIzaSyCcYlZfS6U1rFYkA53kosgxoSxejON7tnI",
-  authDomain: "portfolio-5433f.firebaseapp.com",
-  databaseURL: "https://portfolio-5433f.firebaseio.com",
-  projectId: "portfolio-5433f",
-  storageBucket: "portfolio-5433f.appspot.com",
-  messagingSenderId: "1074864665662"
-};
+const portfolio = {}
 
-firebase.initializeApp(config);
+portfolio.page = 'home';
 
-let ref = firebase.database().ref('projects')
+portfolio.replaceContent = () => {
+  $('header a').on('click', function() {
+    portfolio.page = $(this).text()
+    console.log(portfolio.page)
+    if (portfolio.page !== 'home') {
+      $('header').addClass('header__alt');
+    }
+  })
+}
 
-ref.on('value', snapshot => {
-  console.log(snapshot.val())
-})
+portfolio.scrollAnimations = () => {
+  $('.projects figure').each(function() {
+      let el = $(this)
+      $(window).scroll(() => {
+        let topOffset = el.offset().top
+        let trigger = topOffset;
+        let position = $(window).scrollTop() + ($(window).height()/1.3);
+        if (position >= trigger) {
+          $(this).addClass('fade-in')
+        }
+        if (position < trigger) {
+          $(this).removeClass('fade-in')
+        }
+    })
+  });
+}
+
+portfolio.floatLogo = () => {
+  $(window).on('mousemove', function (e) {
+    let x = (e.clientX / $(window).width());
+    let y = (e.clientY / $(window).height());
+    $('.header__logo')
+      .css('transform', `translate(${x}rem, ${y}rem)`)
+  })
+}
+
+portfolio.init = () => {
+  portfolio.replaceContent();
+  portfolio.floatLogo();
+  portfolio.scrollAnimations();
+}
+
+$(function () {
+  portfolio.init()
+});
